a3cfbdcb207ff890c4f2201539a76d28733e5bd9
Justice Amoah Mensah <justice.amensah48@gmail.com>
2020-05-10 04:57:10 +0000

    upgrade for all files

---
+++ src/main/java/com/amjustice/toastdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.amjustice.toastdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void sayHi(View view) {
+
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+
+        Toast.makeText(MainActivity.this, "Hi there," + nameEditText.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
